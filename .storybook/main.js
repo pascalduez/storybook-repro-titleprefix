debc8544283ea9db3460ffb0895c6846a4414f03
Pascal Duez <public@pascalduez.me>
2022-02-14 19:32:48 +0100

    Demo titlePrefix behaviour

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,24 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  // "stories": [
+  //   "../stories/**/*.stories.mdx",
+  //   "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  // ],
+  stories: [
+    {
+      directory: '../Foo',
+      titlePrefix: 'Foo',
+      files: '**/*.stories.*',
+    },
+    {
+      directory: '../Bar',
+      titlePrefix: 'Bar',
+      files: '**/*.stories.*',
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "framework": "@storybook/react"
-}
+  framework: '@storybook/react',
+};
---
+++ Bar/Switch.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Switch',
+};
+
+export const Switch = () => <p>Bar/Switch</p>;
